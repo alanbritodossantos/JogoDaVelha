d9da7bf125484838356ac3a2226766bd4ceebc77
Alan <alanbritodossantos2015@gmail.com>
2022-06-15 23:56:36 -0300

    criando modulo jogo da velha

--- jogo-da-velha/src/app/app.module.ts
+++ jogo-da-velha/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JogoDaVelhaModule } from './jogo-da-velha';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
